3bd26d9acff506aa2b2206dd5f4549267ed4e2ba
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-27 16:26:17 -0700

    Composing Components

---
+++ src/components/counters.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Counter from "../components/counter";
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 2 },
+      { id: 3, value: 1 },
+      { id: 4, value: 0 },
+    ],
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          {this.state.counters.map((counter) => (
+            <Counter key={counter.id} />
+          ))}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counters;
