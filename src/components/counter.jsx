ea953c40f66c177c1651fb08d28cf3487f3ba6f8
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-26 17:22:52 -0700

    setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,13 +4,25 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imageurl: "https://picsum.photos/200",
+  };
+
+  styles = {
+    fontSize: 20,
+    fontWeight: 500,
+    margin: ".5rem .5rem",
   };
   render() {
     // React.createElement
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <img src={this.state.imageurl} alt="" />
+        <br></br>
+        <br></br>
+        <span style={this.styles} className="badge badge-primary">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
