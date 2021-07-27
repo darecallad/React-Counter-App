74afd82599d839ff7fd2d15b476df9a9fa3ed9f1
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-27 16:35:08 -0700

    set value for each counter (this.props.value)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    value: this.props.value,
     imageurl: "https://picsum.photos/200",
     tags: ["tag1", "tag2", "tag123"],
   };
@@ -18,7 +18,7 @@ class Counter extends Component {
     // pass id event argument
     // make it arrow function
     console.log(id);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
     // use setState from Components class to tell react to update it
   };
   //   style sheet
@@ -65,13 +65,13 @@ class Counter extends Component {
   }
   getBadgeClass() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
     // <h1>Zero</h1>
   }
 }
