3565c258b947d0894e665518f4245fd5f2fb2cda
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-27 10:05:41 -0700

    Rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,7 @@ class Counter extends Component {
   state = {
     count: 1,
     imageurl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
@@ -23,6 +24,11 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}> {tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
